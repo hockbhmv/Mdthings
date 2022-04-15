1a4ff4b3fb1289e9b5a8f933e3b79132ac8a3a74
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2022-04-15 18:40:15 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os 
+import setuptools 
+
+def requirements(file="requirements.txt"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf-8") as r:
+            return [i.strip() for i in r]
+    else:
+        return []
+
+
+def readme(file="README.md"):
+    if os.path.isfile(file):
+        with open(file, encoding="utf-8") as r:
+            return r.read()
+    else:
+        return ""
+
+
+setuptools.setup(
+    name="things",
+    version="0.0.1",
+    description="my requirements",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Md movies",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    project_urls={
+        "Tracker": "https://github.com/hockbhmv/things/issues",
+        "Source": "https://github.com/hockbhmv/things"
+    },
+    python_requires=">=3.6",
+    py_modules=['mdmovies'],
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements()
+)
